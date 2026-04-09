a5916d07b90ceda092322a8f05586d96cf5ad268
krishnadhanraj909-blip <krishnadhanraj909@gmail.com>
2026-04-10 01:52:07 +0530

    Create mobile/screens.py with Kivy screens for app pages

---
+++ mobile/screens.py
@@ -0,0 +1,16 @@
+from kivy.uix.screenmanager import Screen, ScreenManager
+
+class HomeScreen(Screen):
+    pass
+
+class BirthChartScreen(Screen):
+    pass
+
+class HoroscopeScreen(Screen):
+    pass
+
+class ProfileScreen(Screen):
+    pass
+
+class MyScreenManager(ScreenManager):
+    pass
